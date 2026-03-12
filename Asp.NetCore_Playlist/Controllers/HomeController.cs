a7cf10df5d036764c463ba990e1cbecbef77b913
BuildWithCare01 <mayankpal7424@gmail.com>
2026-03-12 21:02:42 +0530

    Lecture :- ASP NET core project file

---
+++ Asp.NetCore_Playlist/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Asp.NetCore_Playlist.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Asp.NetCore_Playlist.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Asp.NetCore_Playlist/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Asp.NetCore_Playlist.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
